707ca3e4ef6bffbf7412b4a3130c104c3fa4a4b8
tinysrc <tinysrc@163.com>
2021-05-07 11:32:21 +0800

    add z9.NewUUID func

--- pkg/z9/utils.go
+++ pkg/z9/utils.go
@@ -1,4 +1,17 @@
 package z9
 
+import (
+	"github.com/gofrs/uuid"
+)
+
 // H is a shortcut for map[string]interface{}
 type H map[string]interface{}
+
+// NewUUID wrap
+func NewUUID() uuid.UUID {
+	uuid, err := uuid.NewV4()
+	if err != nil {
+		panic(err)
+	}
+	return uuid
+}
