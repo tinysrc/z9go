b767e27244de76547b13dd7987513ca2845687ae
tinysrc <tinysrc@163.com>
2021-06-21 15:33:42 +0800

    echo example

--- examples/echo/cmd/client/main.go
+++ examples/echo/cmd/client/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"strconv"
 
 	"github.com/tinysrc/z9go/examples/echo/pb"
@@ -19,7 +20,7 @@ func main() {
 	defer conn.Close()
 	echoService := pb.NewEchoServiceClient(conn)
 	for i := 0; i < 10; i++ {
-		out, err := echoService.Echo(cli.NewCallCtx(), &pb.StringMessage{Value: strconv.Itoa(i)})
+		out, err := echoService.Echo(context.Background(), &pb.StringMessage{Value: strconv.Itoa(i)})
 		if err != nil {
 			log.Error("call echo failed", zap.Error(err))
 		} else {
--- examples/echo/cmd/server/main.go
+++ examples/echo/cmd/server/main.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/tinysrc/z9go/examples/echo/pb"
 	"github.com/tinysrc/z9go/pkg/conf"
 	"github.com/tinysrc/z9go/pkg/log"
+	"github.com/tinysrc/z9go/pkg/naming/registry"
+	"github.com/tinysrc/z9go/pkg/naming/registry/etcd"
 	"github.com/tinysrc/z9go/pkg/rpc"
+	etcd3 "go.etcd.io/etcd/client/v3"
 	"go.uber.org/zap"
+	"google.golang.org/grpc/metadata"
 )
 
 type service struct {
@@ -21,8 +32,61 @@ func (s *service) Echo(ctx context.Context, in *pb.StringMessage) (*pb.StringMes
 
 func main() {
 	defer log.Close()
-	s := rpc.NewServer(nil)
-	pb.RegisterEchoServiceServer(s.Server, &service{})
-	addr := conf.Global.GetString("service.addr")
-	s.Run(addr)
+	// init etcd
+	cfg := etcd3.Config{
+		Endpoints: conf.Global.GetStringSlice("etcd.endpoints"),
+	}
+	si := &registry.ServiceInfo{
+		Id:       conf.Global.GetString("service.id"),
+		Name:     conf.Global.GetString("service.name"),
+		Version:  conf.Global.GetString("service.version"),
+		Metadata: metadata.Pairs("weight", conf.Global.GetString("service.weight")),
+	}
+	reg, err := etcd.NewRegistrar(&etcd.Config{
+		Config: cfg,
+		Dir:    "/z9/backend/services",
+		TTL:    10 * time.Second,
+	})
+	if err != nil {
+		panic(err)
+	}
+	// init rpc server
+	svr := rpc.NewServer(nil)
+	svc := &service{}
+	pb.RegisterEchoServiceServer(svr.Server, svc)
+	// init rpc gateway
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	mux := runtime.NewServeMux()
+	pb.RegisterEchoServiceHandlerServer(ctx, mux, svc)
+	// run
+	wg := sync.WaitGroup{}
+	// run rpc server
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		addr := conf.Global.GetString("service.addr")
+		svr.Run(addr)
+	}()
+	// run rpc gateway
+	go func() {
+		defer wg.Done()
+		addr := conf.Global.GetString("service.gwaddr")
+		http.ListenAndServe(addr, mux)
+	}()
+	// run etcd registrar
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		reg.Register(si)
+	}()
+	// signal
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGTERM, syscall.SIGINT)
+	<-signalChan
+	// etcd unregister
+	reg.Unregister(si)
+	cancel()
+	svr.Stop()
+	wg.Wait()
 }
